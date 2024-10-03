5d6e2cccc1055f5bb6c210d4e78fe53e3fee686b
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-10-03 21:44:51 +0530

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,50 +1,38 @@
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import { HomeLayout } from '../components/Home-Layout';
 import { useUserContext } from '../context/UserContext';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import useModel from '../hooks/useModel';
 import { usePhotoContext } from '../context/PhotoContext';
-import dynamic from 'next/dynamic';
+import useModel from '../hooks/useModel';
 
-// dynamic imports
+// Dynamic imports
 const Alert = dynamic(() => import('../components/UI/Alert'));
 const UserCard = dynamic(() => import('../components/Block/User_Card'));
 const MyPhotos = dynamic(() => import('../components/Section/My-Photos'));
-const EditProfileModel = dynamic(() =>
-  import('../components/Models/Edit-Profile')
-);
+const EditProfileModel = dynamic(() => import('../components/Models/Edit-Profile'));
 const AppLayout = dynamic(() => import('../components/App-Layout'));
-
 const MainNavbar = dynamic(() => import('../components/Section/Navbar/Navbar'));
 
 export default function MyProfile() {
   const {
-    authState: {
-      isAuthenticate,
-      loading,
-      auth: { user }
-    },
-    dispatchAuthenticate
+    authState: { isAuthenticate, loading, auth: { user } },
+    dispatchAuthenticate,
   } = useUserContext();
 
-  const {
-    photoState: { photosError },
-    useRemoveError
-  } = usePhotoContext();
-
+  const { photoState: { photosError }, useRemoveError } = usePhotoContext();
   const { isModel, openModel, closeModel } = useModel();
-
   const router = useRouter();
 
   useEffect(() => {
     dispatchAuthenticate();
-  }, [isAuthenticate]);
+  }, [isAuthenticate, dispatchAuthenticate]);
 
   useEffect(() => {
-    if (isAuthenticate !== null) {
-      !isAuthenticate && router.push('/');
+    if (!loading && isAuthenticate === false) {
+      router.push('/');
     }
-  }, [loading]);
+  }, [loading, isAuthenticate, router]);
 
   return (
     <AppLayout page="profile">
@@ -52,14 +40,7 @@ export default function MyProfile() {
       <HomeLayout>
         <MainNavbar />
         {user && (
-          <div
-            className="content"
-            style={{
-              width: '100%',
-              margin: '3rem auto',
-              display: 'flex'
-            }}
-          >
+          <div className="content" style={{ width: '100%', margin: '3rem auto', display: 'flex' }}>
             <UserCard
               userName={user.displayName}
               userImage={user.photoURL}
