439a50a5bd86ff40941e87e81d5c7b908e9da95e
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-10-03 21:43:19 +0530

    destructured addUser.js

--- firebase/addUser.js
+++ firebase/addUser.js
@@ -1,26 +1,19 @@
 import { findUser } from './findUser';
 import { fireDb } from './firebaseConfig';
 
-export const addUser = async userData => {
-  const { displayName, email, photoURL, uid } = userData;
+export const addUser = async ({ displayName, email, photoURL, uid }) => {
   const doc = await fireDb.collection('users').add({
-    displayName: displayName ? displayName : email.split('@')[0],
-    email: email,
+    displayName: displayName || email.split('@')[0],
+    email,
     UID: uid,
-    photoURL: photoURL
-      ? photoURL
-      : 'https://firebasestorage.googleapis.com/v0/b/photo-sight.appspot.com/o/default_user.png?alt=media&token=93e215bf-4b99-4dde-9127-21eefb65df16'
+    photoURL: photoURL || 'https://firebasestorage.googleapis.com/v0/b/photo-sight.appspot.com/o/default_user.png?alt=media&token=93e215bf-4b99-4dde-9127-21eefb65df16',
   });
 
-  const snapshot = await fireDb.collection('users').doc(doc.id).get();
-  return await snapshot.data();
+  const snapshot = await doc.get();
+  return snapshot.data();
 };
 
 export const addSocialUser = async userData => {
   const isUser = await findUser(userData.uid);
-  if (!isUser.data) {
-    return await addUser(userData);
-  } else {
-    return isUser;
-  }
+  return isUser.data ? isUser : await addUser(userData);
 };
