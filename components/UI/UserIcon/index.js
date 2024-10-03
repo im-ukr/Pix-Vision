ac658a75737a4d1973c29df13a546a7e98bff065
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-10-03 21:42:07 +0530

    improved prop handling

--- components/UI/UserIcon/index.js
+++ components/UI/UserIcon/index.js
@@ -1,4 +1,4 @@
-import Styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const waveEffect = keyframes`
@@ -6,67 +6,65 @@ const waveEffect = keyframes`
     box-shadow: 0 0 0 0px rgba(235,20,97,0.2);
   }
   100% {
-    box-shadow: 0 0 0 5px  rgba(235,20,97,0);
+    box-shadow: 0 0 0 5px rgba(235,20,97,0);
   }
 `;
 
-export const Icon = Styled.div`
-    height: 30px;
-    position: relative;
-    width: 30px;
+const IconWrapper = styled.div`
+  height: 30px;
+  width: 30px;
+  position: relative;
+  border-radius: 50%;
+  box-shadow: 0 0 0 1px ${(props) => props.stroke || '#0009'};
+
+  .img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
     border-radius: 50%;
-    box-shadow: 0 0 0 1px ${props => (props.stroke ? props.stroke : '#0009')};
-
-    .img {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-        border-radius: 50%;
-        border: 2px solid #0000;
-    }
-
+    border: 2px solid transparent;
+  }
 `;
 
-export const IconLink = Styled.section`
-    color: #000d;
-    cursor: pointer;
+const IconLink = styled.section`
+  color: #000d;
+  cursor: pointer;
 
-    &:not(:last-child){
-        margin-right: 30px;
-    }
+  &:not(:last-child) {
+    margin-right: 30px;
+  }
 
-    .svg {
-        height: 25px;
-        width: 25px; 
-    }   
+  .svg {
+    height: 25px;
+    width: 25px;
+  }
 `;
 
-export const CardIcon = Styled.button`
+const CardIcon = styled.button`
   color: #000d;
   background-color: transparent;
   border: none;
   cursor: pointer;
 
-    &:not(:last-child){
-        margin-right: 10px;
-        
-    }
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
 
-    .svg {
-        height: 22px;
-        width: 22px;
-        border-radius: 10px;
-    }  
-    
-    .like {
-      animation: ${waveEffect} 0.3s ease-out; 
-    }
+  .svg {
+    height: 22px;
+    width: 22px;
+    border-radius: 10px;
+  }
+
+  .like {
+    animation: ${waveEffect} 0.3s ease-out;
+  }
 `;
 
-export const UserIcon = ({ img, stroke }) => {
-  return (
-    <Icon stroke={stroke}>
-      <LazyLoadImage src={img} layout="fill" className="img" alt="user icon" />
-    </Icon>
-  );
-};
+export const UserIcon = ({ img, stroke = '#0009' }) => (
+  <IconWrapper stroke={stroke}>
+    <LazyLoadImage src={img} className="img" alt="user icon" />
+  </IconWrapper>
+);
+
+export { IconLink, CardIcon };
