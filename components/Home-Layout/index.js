23a76ee25faae8ee3e44a0175cf4d1cfc3a35c3f
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-10-03 21:39:40 +0530

    Update index.js

--- components/Home-Layout/index.js
+++ components/Home-Layout/index.js
@@ -1,17 +1,11 @@
-import Styled from 'styled-components';
+import styled from 'styled-components';
 import useModel from '../../hooks/useModel';
 import SharePhoto from '../Models/Share-Photo';
 import AddPostButton from '../UI/Post-Button';
 
-const Layout = Styled.div`
-  padding: 20px 0;
-  max-width: 1400px;
-  margin: 0 auto;
-  position: relative;
-`;
-
 export const HomeLayout = ({ children }) => {
   const { isModel, closeModel, openModel } = useModel();
+
   return (
     <Layout>
       {isModel && <SharePhoto closeModel={closeModel} />}
@@ -20,3 +14,9 @@ export const HomeLayout = ({ children }) => {
     </Layout>
   );
 };
+const Layout = styled.div`
+  padding: 20px 0;
+  max-width: 1400px;
+  margin: 0 auto;
+  position: relative;
+`;
